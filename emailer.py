043a7416363f095f25d27e71c948e2c00182418a
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2019-12-14 17:26:55 -0800

    Tested and commented

--- emailer.py
+++ emailer.py
@@ -2,17 +2,25 @@ import smtplib
 
 smtp = smtplib.SMTP("smtp.gmail.com", 587)
 
+# Required to setup the server
 smtp.ehlo()
-
 smtp.starttls()
 
 user = input("Username: ")
+# For Gmail this is probably an app password
 pswd = input("Password: ")
 
+# Authenticates the user
 smtp.login(user, pswd)
 
-smtp.sendmail("MY_EMAIL_ADDRESS", "recipient_email_address", "Subject: So long.\n body of the mssage")
+source = input("Who is sending: ")
+target = input("Who is receiving: ")
 
+# For some reason there could not be a space between the \n and the next letter
+# Could probably be better by just using triple quote messages
+smtp.sendmail(source, target, "Subject: So long.\nDear Alice, so long and thanks for all the fish. Sincerely, Bob")
+
+# Closes the server
 smtp.quit()
 
 
